a48af4030e03ca72df96f5c1a091184ba1c36776
Plkylover <101154460+Plkylover@users.noreply.github.com>
2024-01-11 07:29:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let sum = ""
+let digits = ""
+const screen = document.getElementById("display")
+const clearBtn = document.getElementById("clear")
+const clear = () => {
+    screen.innerHTML = " "
+    sum = ""
+    digits = ""
+}
+clearBtn.addEventListener("click", clear)
+const addline = (arg, type) => {
+    let access = false
+    if (type == 'opra' && (digits !== "" || arg == "-")) {
+        digits = ""
+        access = true
+    }
+    if (type == "num") {
+        if ((digits.includes(".") && arg !== ".") || !(digits.includes("."))) {
+            digits += arg
+            access = true
+        }
+    }
+    if (access) {
+        if ((sum[sum.length - 1] == "+" || sum[sum.length - 1] == "-" || sum[sum.length - 1] == "*" || sum[sum.length - 1] == "/") && (arg == "+" || arg == "-" || arg == "×" || arg == "÷")) {
+            screen.innerHTML = screen.innerHTML.slice(0, screen.innerHTML.length - 1)
+            sum = sum.slice(0, sum.length - 1)
+        }
+        screen.innerHTML += arg
+        if (arg == "×") {
+            arg = "*"
+        }
+        if (arg == "÷") {
+            arg = "/"
+        }
+        sum += arg
+    }
+}
+const calculate = () => {
+    try {
+        const answer = eval(sum)
+        if (answer !== undefined) {
+            screen.innerHTML = answer
+        }
+    }
+    catch { }
+}
